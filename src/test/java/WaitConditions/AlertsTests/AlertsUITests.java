493d05841c4e9dfe37483c6f0060c645f8fbcfd8
volodymyrprydatko <uarealtek1994@gmail.com>
2024-09-27 21:45:50 +0300

    Added UI Tests for prompts

--- src/test/java/WaitConditions/AlertsTests/AlertsUITests.java
+++ src/test/java/WaitConditions/AlertsTests/AlertsUITests.java
@@ -6,6 +6,9 @@ import WaitConditions.Helper.AlertsBaseOperations;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,7 +18,8 @@ public class AlertsUITests extends BaseTest {
     public void checkAlertData() {
         AlertsBaseOperations.showAlert(0,2); // Assuming the alert will be displayed in the first 2 seconds
 
-        Alert alert = BaseOperations.getWait().until(ExpectedConditions.alertIsPresent());
+        WebDriverWait alertWait = new WebDriverWait(BaseOperations.getDriver(), Duration.ofSeconds(2));
+        Alert alert = alertWait.until(ExpectedConditions.alertIsPresent());
         BaseOperations.getDriver().switchTo().alert();
 
         assertEquals("I am alerting you!",
---
+++ src/test/java/WaitConditions/PromptsTests/PromptsUITests.java
@@ -0,0 +1,30 @@
+package WaitConditions.PromptsTests;
+
+import SamplePages.Helper.BaseOperations;
+import SamplePages.Helper.BaseTest;
+import WaitConditions.Helper.PromptBaseOperations;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PromptsUITests extends BaseTest {
+
+    @Test
+    public void checkPromptData() {
+        PromptBaseOperations.showPrompt(2,5); // Assuming minimum delay - 2 seconds, maximum - 5 seconds
+
+        WebDriverWait alertWait = new WebDriverWait(BaseOperations.getDriver(), Duration.ofSeconds(5));
+        Alert alert = alertWait.until(ExpectedConditions.alertIsPresent());
+        BaseOperations.getDriver().switchTo().alert();
+
+        assertEquals(alert.getText(),"Choose wisely...\n" +
+                "It's your life!", "Expected the message but got " + alert.getText());
+
+        alert.accept();
+    }
+}
