e335f3e3134a1a8f6bdd58a7c35d02ec57522aee
Your Name <uarealtek1994@gmail.com>
2024-12-26 21:52:09 +0200

    finished the radiobuttons tests

--- src/test/java/Tests/FormsPage/RadiobuttonsTests.java
+++ src/test/java/Tests/FormsPage/RadiobuttonsTests.java
@@ -12,6 +12,8 @@ import org.openqa.selenium.WebElement;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Execution(ExecutionMode.CONCURRENT)
 public class RadiobuttonsTests extends DriverOperations {
@@ -20,10 +22,9 @@ public class RadiobuttonsTests extends DriverOperations {
     public void verifyLabelForAttributeMatchesInputId() {
         SoftAssertions soft = new SoftAssertions();
         BaseOperations.navigateTo(URLs.FORMS_PAGE);
-        FormsPage page = new FormsPage(getDriver());
 
         List<WebElement> radiobuttons = FormsPage.getListOfRadioButtons();
-        List<WebElement> radiobuttonLabels = page.getListOfRadioButtonLabels();
+        List<WebElement> radiobuttonLabels = FormsPage.getListOfRadioButtonLabels();
         List<String> radiobuttonLabelAttributes = new ArrayList<>();
 
         for (WebElement label : radiobuttonLabels) {
@@ -128,45 +129,30 @@ public class RadiobuttonsTests extends DriverOperations {
     public void checkRadiobuttonSelectionViaLabel() {
         SoftAssertions soft = new SoftAssertions();
         BaseOperations.navigateTo(URLs.FORMS_PAGE);
-        FormsPage page = new FormsPage(getDriver());
 
         List<WebElement> radiobuttons = FormsPage.getListOfRadioButtons();
         List<WebElement> radiobuttonLabels = FormsPage.getListOfRadioButtonLabels();
 
-        //Checking that for each RadiobuttonLabel there's a corres
+        //Checking that for each RadiobuttonLabel there's a corresponding radiobutton
         for (WebElement label : radiobuttonLabels) {
             String currentLabelForAttribute = label.getAttribute("for");
-            boolean isCorrespondingButtonPresent = radiobuttons.stream()
-                            .anyMatch(r -> r.getAttribute("id")
-                            .equals(currentLabelForAttribute));
+            Optional<WebElement> correspondingButton = radiobuttons.stream()
+                            .filter(r -> Objects.equals(r.getAttribute("id"), currentLabelForAttribute))
+                            .findFirst();
 
             //Clicking the label if its corresponding button is FOUND in the radiobuttons list
-            if (isCorrespondingButtonPresent){
+            if (correspondingButton.isPresent()) {
                 label.click();
+                soft.assertThat(Objects.equals(correspondingButton.get()
+                                .getAttribute("checked"), "true")
+                                && correspondingButton.get().isSelected())
+                                .isTrue();
+            } else {
+                soft.fail("the match between button and label was NOT found");
             }
 
-            List<WebElement> unselectedButtons = new ArrayList<>();
-            for (WebElement radiobutton : radiobuttons) {
-                if ((!radiobutton.isSelected()) && radiobutton.getAttribute("checked") == null) {
-                    unselectedButtons.add(radiobutton);
-                }
-            }
-
-            //Check that all other buttons are unselected after clicking on the label
-            soft.assertThat(unselectedButtons.size() == radiobuttons.size() -1).isTrue();
-
+            soft.assertAll();
         }
-        
     }
-
-    /**
-     * check that after clicking on the label the radiobutton is being selected
-     *
-     * TODO:
-     * Verify that for each specific radiobutton, theres a corresponding label in other list
-     * (work with labels)
-     * Click that label
-     * Check that all other radiobuttons in the list are unchecked
-     */
 
 }
