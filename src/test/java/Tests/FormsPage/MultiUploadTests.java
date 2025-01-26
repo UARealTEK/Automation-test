131967166a52af6192a02883befc54f7cc060e5c
Your Name <uarealtek1994@gmail.com>
2025-01-26 13:59:35 +0200

    Finished Tests for FileUpload

--- src/test/java/Tests/FormsPage/MultiUploadTests.java
+++ src/test/java/Tests/FormsPage/MultiUploadTests.java
@@ -5,11 +5,20 @@ import Enums.URLs;
 import Pages.FormsPage.MultiUpload.MultiUpload;
 import Utils.BaseOperations;
 import Utils.DriverOperations;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Test;
 
 public class MultiUploadTests extends DriverOperations {
 
+    private static final Logger log = LogManager.getLogger(MultiUploadTests.class);
+
+    /**
+     * TODO:
+     * - create method for verifying the match between label and uploaded files
+     * - create a method that randomly selects files for uploading and apply it in the test
+     */
     @Test
     public void checkMultiFileUploadDefaultState() {
         SoftAssertions soft = new SoftAssertions();
@@ -23,6 +32,11 @@ public class MultiUploadTests extends DriverOperations {
         soft.assertAll();
     }
 
+    /**
+     * TODO:
+     * - create method for verifying the match between label and uploaded files
+     * - create a method that randomly selects files for uploading and apply it in the test
+     */
     @Test
     public void checkMultiFileUploadProcess() {
         SoftAssertions soft = new SoftAssertions();
@@ -32,6 +46,9 @@ public class MultiUploadTests extends DriverOperations {
         page.fileUploadMulti(Files.WINDOWS_FILE_1,Files.WINDOWS_FILE_2);
 
         soft.assertThat(page.isElementAndLabelMatched()).isTrue();
+        soft.assertThat(page.getMultiFileUploadStateLabel().isEmpty()).isFalse();
+        log.debug(page.getMultiFileUploadStateLabel());
+
         soft.assertAll();
     }
 }
