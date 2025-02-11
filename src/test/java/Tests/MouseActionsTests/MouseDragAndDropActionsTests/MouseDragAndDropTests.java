cefe3f0c783c485321b46ec2527072e194754d27
Your Name <uarealtek1994@gmail.com>
2025-02-11 22:13:21 +0200

    updating

--- src/test/java/Tests/MouseActionsTests/MouseDragAndDropActionsTests/MouseDragAndDropTests.java
+++ src/test/java/Tests/MouseActionsTests/MouseDragAndDropActionsTests/MouseDragAndDropTests.java
@@ -4,11 +4,15 @@ import Enums.URLs;
 import Pages.MouseActionsPage.MouseDragAndDropActions;
 import Utils.BaseOperations;
 import Utils.DriverOperations;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 public class MouseDragAndDropTests extends DriverOperations {
+
+    private static final Logger log = LogManager.getLogger(MouseDragAndDropTests.class);
 
     @Test
     public void checkDefaultState() {
@@ -17,6 +21,7 @@ public class MouseDragAndDropTests extends DriverOperations {
         MouseDragAndDropActions page = new MouseDragAndDropActions(getDriver());
 
         soft.assertThat(page.getDragValidate().getText()).isEqualTo(page.getExpectedDefaultDragTargetText());
+        log.info(page.getDragValidate());
 
         soft.assertAll();
     }
